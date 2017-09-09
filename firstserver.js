eba1295133e16a4088c8afc1ff724ded4cc7e8df
wesklotzly <stuwn420@gmail.com>
2017-09-09 11:27:51 -0700

    Add files via upload

---
+++ firstserver.js
@@ -0,0 +1,51 @@
+// Here we require/import the HTTP module
+var http = require("http");
+
+// Here we define a port to listen to
+var PORT = 7000;
+var PORT2 = 7500;
+
+
+// Here we create a generic function to handle requests and responses
+function handleRequest(request, response) {
+
+  // The below statement is triggered (client-side) when the user visits the PORT URL
+  response.end("Wes is dope af " + request.url);
+}
+
+// Here we use the Node HTTP package to create our server.
+// We then pass it the handleRequest function to empower it with functionality.
+var server = http.createServer(handleRequest);
+
+// Here we start our server so that it can begin listening to client requests.
+server.listen(PORT, function() {
+
+  // The below statement is triggered (server-side) when a user visits the PORT URL
+  console.log("Server listening on: http://localhost:%s", PORT);
+
+});
+
+
+
+function handleRequest(request, response) {
+
+  // The below statement is triggered (client-side) when the user visits the PORT URL
+  response.end("Wes is super lame " + request.url);
+}
+
+// Here we use the Node HTTP package to create our server.
+// We then pass it the handleRequest function to empower it with functionality.
+var server = http.createServer(handleRequest);
+
+// Here we start our server so that it can begin listening to client requests.
+server.listen(PORT2, function() {
+
+  // The below statement is triggered (server-side) when a user visits the PORT URL
+  console.log("Server listening on: http://localhost:%s", PORT2);
+
+});
+
+
+
+
+
